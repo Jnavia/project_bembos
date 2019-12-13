2e6547e3560135e132cdf16ad8c02924ba8a6dba
Jnavia <54745830+Jnavia@users.noreply.github.com>
2019-12-13 17:43:03 -0500

    Add files via upload

--- apps/orders/viewsets.py
+++ apps/orders/viewsets.py
@@ -8,24 +8,24 @@ from rest_framework.authentication import TokenAuthentication
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class=CategorySerializer
     queryset = Category.objects.all()
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
 class SupplyViewSet(viewsets.ModelViewSet):
     serializer_class=SupplySerializer
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
     def get_queryset(self):
@@ -38,17 +38,17 @@ class SupplyViewSet(viewsets.ModelViewSet):
 class Order_StateViewSet(viewsets.ModelViewSet):
     serializer_class=Order_StateSerializer
     queryset = Order_State.objects.all()
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class=OrderSerializer
     queryset = Order.objects.all()
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
 
 class OrderDetailViewSet(viewsets.ModelViewSet):
     serializer_class=OrderDetailSerializer
     queryset = OrderDetail.objects.all()
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     authentication_class = (TokenAuthentication,)
